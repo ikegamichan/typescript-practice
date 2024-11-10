13d492aa27e00d59d9f9b965977b619916118263
ikegamichan <ikegamitakuto@gmail.com>
2024-11-10 22:21:54 +0900

    layout,pageの作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import './globals.css';
+import '../../dist/output.css';
+import React from 'react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <header>
-          <h1>practice-typescript</h1>
+          <h1 className="text-red-500" style={{ color: 'red' }}>
+            practice-typescript
+          </h1>
         </header>
         {children}
       </body>
