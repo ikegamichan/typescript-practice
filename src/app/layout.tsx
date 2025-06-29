4f8a44bb84147c32a6f51683fb5f79ddf9ef4640
ikegamichan <ikegamitakuto@gmail.com>
2025-06-29 23:55:02 +0900

    layout.tsxの修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import './globals.css';
-import '../../dist/output.css';
 import React from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <header>
-          <h1 className="text-red-500" style={{ color: 'red' }}>
-            practice-typescript
-          </h1>
-        </header>
-        {children}
+        <div className="mx-auto mt-8 max-w-4xl">{children}</div>
       </body>
     </html>
   );
