eaf2f27e8d0e9796f9e768d81b7f49561f7863ed
ikegamichan <ikegamitakuto@gmail.com>
2025-06-29 23:54:27 +0900

    設定の見直し パッケージの追加

--- eslint-configs/typescript.js
+++ eslint-configs/typescript.js
@@ -3,11 +3,11 @@
  * {@link https://typescript-eslint.io/rules}.
  */
 module.exports = {
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
       },
@@ -15,16 +15,16 @@ module.exports = {
   },
   rules: {
     // 返り値の推論を許容する
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     // @ts-ignoreのコメント禁止。@ts-expect-errorを利用し、説明を記載するように警告を出す
-    "@typescript-eslint/ban-ts-comment": "warn",
+    '@typescript-eslint/ban-ts-comment': 'warn',
     // no-unused-imports で import 除去を設定するため無効化
-    "@typescript-eslint/no-unused-vars": "off",
+    '@typescript-eslint/no-unused-vars': 'off',
     // import type への振り分けをすることで視認性の向上を行う
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
       {
-        prefer: "type-imports",
+        prefer: 'type-imports',
       },
     ],
   },
