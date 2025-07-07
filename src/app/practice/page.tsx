e581ddbfdf179329ec242f2ee1215fb686167cfd
ikegamichan <ikegamitakuto@gmail.com>
2025-07-07 23:09:34 +0900

    fix : Cardコンポーネントの実装変更 isDisabled属性を追加

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -8,7 +8,15 @@ export default function Page() {
       <Header title="勉強中のもの" size="L" />
       <div className="grid grid-cols-3 gap-4">
         {titleCards.map((titleCard) => {
-          return <Card key={titleCard.title} title={titleCard.title} href={titleCard.path} size={titleCard.size} />;
+          return (
+            <Card
+              key={titleCard.title}
+              title={titleCard.title}
+              href={titleCard.path}
+              size={titleCard.size}
+              isDisabled={false}
+            />
+          );
         })}
       </div>
     </div>
