e4a14861fed39df67952b430de2ab88956406089
ikegamichan <ikegamitakuto@gmail.com>
2025-07-07 23:11:18 +0900

    cardコンポーネントの実装を変更

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,6 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
 import type { CardSize } from '@/const/card';
 import { cn } from '@/utils/cn';
 
@@ -6,20 +8,27 @@ type Props = {
   size: CardSize;
   title: string;
   href: string;
+  isDisabled: boolean;
 };
 
-export const Card = ({ title, href, size }: Props) => {
+export const Card = ({ title, href, size, isDisabled }: Props) => {
+  const { push } = useRouter();
+  const disabledStyle = isDisabled
+    ? 'bg-gray-500 text-gray-100'
+    : 'cursor-pointer transition duration-500 ease-in-out hover:bg-gray-100';
   const sizeStyle = size === 'm' ? 'h-24 w-auto text-2xl' : size === 's' ? 'h-20 w-24 text-sm' : '';
   return (
-    <Link href={href}>
-      <div
-        className={cn(
-          sizeStyle,
-          'my-auto grid cursor-pointer items-center rounded-lg border border-gray-300 p-2 text-center shadow-md transition duration-500 ease-in-out hover:bg-gray-100',
-        )}
-      >
-        <div className="font-bold">{title}</div>
-      </div>
-    </Link>
+    <div
+      onClick={() => {
+        push(href);
+      }}
+      className={cn(
+        sizeStyle,
+        disabledStyle,
+        'my-auto grid items-center rounded-lg border border-gray-300 p-2 text-center shadow-md',
+      )}
+    >
+      <div className="font-bold">{title}</div>
+    </div>
   );
 };
