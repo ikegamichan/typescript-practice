0473c760e6b6590f93c06aa52cd84d966b2414a3
ikegamichan <ikegamitakuto@gmail.com>
2025-07-07 23:03:41 +0900

    practiceディレクトリを整理

--- src/app/practice/layout.tsx
+++ src/app/practice/layout.tsx
@@ -4,7 +4,7 @@ import { Slider } from '@/components/slider';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="grid min-h-screen grid-cols-1 grid-rows-[1fr_auto] px-4 py-8">
-      <div>{children}</div>
+      <div className="pb-8">{children}</div>
       <div className="relative w-full px-8">
         <Slider />
       </div>
